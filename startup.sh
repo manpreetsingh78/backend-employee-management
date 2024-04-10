232935aafa6197058b5717fb3d8561efaabed32e
manpreet <18bcs6709@cuchd.in>
2024-04-10 21:39:36 +0530

    minor change

--- startup.sh
+++ startup.sh
@@ -12,7 +12,7 @@ sudo yum install postgresql12 postgresql12-server
 sudo /usr/pgsql-12/bin/postgresql-12-setup initdb
 sudo systemctl enable --now postgresql-12
 Created symlink from /etc/systemd/system/multi-user.target.wants/postgresql-12.service to /usr/lib/systemd/system/postgresql-12.service.
-sudo -u postgres psql && CREATE DATABASE employee;Alter USER postgres WITH PASSWORD 'admin123';ALTER ROLE postgres SET client_encoding TO 'utf8';ALTER ROLE postgres SET default_transaction_isolation TO 'read committed';ALTER ROLE postgres SET timezone TO 'UTC';GRANT ALL PRIVILEGES ON DATABASE employee TO postgres;
+sudo -u postgres psql && CREATE DATABASE employee;Alter USER postgres WITH PASSWORD 'admin123';ALTER ROLE postgres SET client_encoding TO 'utf8';ALTER ROLE postgres SET default_transaction_isolation TO 'read committed';ALTER ROLE postgres SET timezone TO 'UTC';GRANT ALL PRIVILEGES ON DATABASE employee TO postgres;exit;
 python manage.py makemigrations
 python manage.py migrate 
 python manage.py collectstatic && gunicorn --workers 2 employee_manage.wsgi
