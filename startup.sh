dc258cabc4ba1896f64706a8abed541e8d79335c
manpreet <18bcs6709@cuchd.in>
2024-04-10 22:23:35 +0530

    changed minor

--- startup.sh
+++ startup.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-python manage.py makemigrations
-python manage.py migrate 
-python manage.py collectstatic
-gunicorn --workers 2 employee_manage.wsgi
+virtual venv && source venv/bin/activate
+python manage.py makemigrations && python manage.py migrate 
+python manage.py collectstatic && gunicorn --workers 2 employee_manage.wsgi
