7f412aa2763a22202516b9ab4146876a37ac6ca3
manpreet <18bcs6709@cuchd.in>
2024-04-10 21:19:25 +0530

    minor change

--- startup.sh
+++ startup.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-python manage.py collectstatic && gunicorn --workers 2 employee_manage.wsgi
+python manage.py collectstatic && python manage.py makemigrations && python manage.py migrate && gunicorn --workers 2 employee_manage.wsgi
