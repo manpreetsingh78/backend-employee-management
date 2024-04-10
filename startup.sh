731958cf6ec2573cefd58e09b9440ffe08089ec2
manpreet <18bcs6709@cuchd.in>
2024-04-10 23:38:04 +0530

    minor error

--- startup.sh
+++ startup.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 virtual venv && source venv/bin/activate
+pip install psycopg2-binary
 python manage.py makemigrations && python manage.py migrate
 python manage.py collectstatic && gunicorn --workers 2 employee_manage.wsgi
