ac8f13c1e74de6a92308b32ac8a5e653cd92229b
manpreet <18bcs6709@cuchd.in>
2024-04-10 20:58:23 +0530

    minor change

--- employee_manage/settings.py
+++ employee_manage/settings.py
@@ -81,7 +81,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleware,WhiteNoiseMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'employee_manage.urls'
