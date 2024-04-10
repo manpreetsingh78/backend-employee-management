93b1ac5b7404199f047c87f858f19d3ba0141986
manpreet <18bcs6709@cuchd.in>
2024-04-10 22:57:28 +0530

    createuser super

--- startup.sh
+++ startup.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 virtual venv && source venv/bin/activate
 pip install psycopg2-binary
-python manage.py makemigrations && python manage.py migrate 
+python manage.py makemigrations && python manage.py migrate
+echo "from django.contrib.auth.models import User; User.objects.create_superuser('boss', 'admin@example.com', 'boss')" | python manage.py shell
 python manage.py collectstatic && gunicorn --workers 2 employee_manage.wsgi
