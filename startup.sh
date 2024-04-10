7bb21d00ad61dc00791f23515787a59c77c8470b
manpreet <18bcs6709@cuchd.in>
2024-04-10 22:28:51 +0530

    m c

--- startup.sh
+++ startup.sh
@@ -1,4 +1,2 @@
 #!/bin/bash
-virtual venv && source venv/bin/activate
-python manage.py makemigrations && python manage.py migrate 
 python manage.py collectstatic && gunicorn --workers 2 employee_manage.wsgi
